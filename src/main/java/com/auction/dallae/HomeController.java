ec9a10bde7ccf8505ecc0eb58d7a92269b8dfa0b
bong <2413bong@naver.com>
2018-11-12 11:56:00 +0900

    테스트용 커밋

--- src/main/java/com/auction/dallae/HomeController.java
+++ src/main/java/com/auction/dallae/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
-	public UserInfoService userInfoService;
+	public UserInfoService userInfoService;     
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model, @ModelAttribute UserInfo userInfo) {
